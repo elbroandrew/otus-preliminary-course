6e33e7d265256676f2871cea2efb14912c552e35
Andrey <elbroandrew@gmail.com>
2021-02-02 12:41:33 +1000

    fixed some eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true,
   },
   extends: [
     'airbnb-base',
   ],
+  "plugins": ["jest"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
--- src/lesson01/index.test.js
+++ src/lesson01/index.test.js
@@ -1,62 +1,62 @@
-import { sumAndMultiplyOutput, strSum, sumOfInputDigits } from "./index";
+import { sumAndMultiplyOutput, strSum, sumOfInputDigits } from './index';
 
-//1
-describe("exercise 1 return sum and multiply of two numbers", () => {
-  it("returns 'obj.sum' to be 5 and 'obj.mul' to be 6 for numbers 2 and 3", () => {
-    let obj = sumAndMultiplyOutput(2, 3);
+// 1
+describe('exercise 1 return sum and multiply of two numbers', () => {
+  it("returns 'obj.sum' to be 5 and 'obj.mul' to be 6 for nums 2 and 3", () => {
+    const obj = sumAndMultiplyOutput(2, 3);
 
     expect(obj.sum).toBe(5);
     expect(obj.mul).toBe(6);
   });
 
   it('returns "not numbers" for strings "str1" and "str2"', () => {
-    const str1 = "abc",
-      str2 = "def";
-    expect(sumAndMultiplyOutput(str1, str2)).toBe("not numbers");
+    const str1 = 'abc';
+    const str2 = 'def';
+    expect(sumAndMultiplyOutput(str1, str2)).toBe('not numbers');
   });
 
   it("returns 'not numbers' for value1 = NaN and value2 = NaN values", () => {
-    const value1 = NaN,
-      value2 = NaN;
-    expect(sumAndMultiplyOutput(value1, value2)).toBe("not numbers");
+    const value1 = NaN;
+    const value2 = NaN;
+    expect(sumAndMultiplyOutput(value1, value2)).toBe('not numbers');
   });
 });
 
-//2
-describe("exercise 2 two strings sum", () => {
+// 2
+describe('exercise 2 two strings sum', () => {
   it("returns 10 for strings 'lorem' and 'ipsum'", () => {
-    expect(strSum("lorem", "ipsum")).toBe(10);
+    expect(strSum('lorem', 'ipsum')).toBe(10);
   });
 
   it('returns "not strings" for numbers 1 and 2', () => {
-    expect(strSum(1, 2)).toBe("not strings");
+    expect(strSum(1, 2)).toBe('not strings');
   });
   it('returns "not strings" for arrays', () => {
-    expect(strSum([], [])).toBe("not strings");
+    expect(strSum([], [])).toBe('not strings');
   });
 });
-//3
-describe("exercise 3, sum of digits in 3-digit number", () => {
+// 3
+describe('exercise 3, sum of digits in 3-digit number', () => {
   it("returns 6 for '123' ", () => {
-    expect(sumOfInputDigits("123")).toBe(6);
+    expect(sumOfInputDigits('123')).toBe(6);
   });
 
   it('returns 6 for "-123"', () => {
-    expect(sumOfInputDigits("-123")).toBe(6);
+    expect(sumOfInputDigits('-123')).toBe(6);
   });
 
   it('returns "12 is not a 3-digit number" ', () => {
-    expect(sumOfInputDigits("12")).toBe("12 is not a 3-digit number");
+    expect(sumOfInputDigits('12')).toBe('12 is not a 3-digit number');
   });
   it('returns "-12 is not a 3-digit number" for "-12"', () => {
-    expect(sumOfInputDigits("-12")).toBe("-12 is not a 3-digit number");
+    expect(sumOfInputDigits('-12')).toBe('-12 is not a 3-digit number');
   });
 
   it('returns "1234 is not a 3-digit number" for "1234"', () => {
-    expect(sumOfInputDigits("1234")).toBe("1234 is not a 3-digit number");
+    expect(sumOfInputDigits('1234')).toBe('1234 is not a 3-digit number');
   });
 
   it('returns "Empty value" for ""', () => {
-    expect(sumOfInputDigits("")).toBe("Empty value");
+    expect(sumOfInputDigits('')).toBe('Empty value');
   });
 });
